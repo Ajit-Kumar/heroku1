b45379d8db0e64189771371dd19665d21d657f2d
Ajit-Kumar <ajit.kumar0493@gmail.com>
2019-12-05 13:17:35 +0000

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -6,5 +6,5 @@ y=[3,4,5,6,7]
 
 fig=figure(plot_height=500,plot_width=1000, tools="save", x_range=[0,10], y_range=[0,10],toolbar_location="below")
 fig.line(x=x,y=y)
-fig.image_url(url=["myapp/statis/bokeh_plot.png"],x=0,y=0,w=100,h=100,anchor="bottom_left")
+fig.image_url(url=["myapp/static/images/base.png"],x=0,y=0,w=100,h=100,anchor="bottom_left")
 show(fig)
