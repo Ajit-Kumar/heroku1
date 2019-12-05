5cb2e6c8896ca83011085d261fc8b4a3e8e058c7
Ajit-Kumar <ajit.kumar0493@gmail.com>
2019-12-05 13:02:12 +0000

    Update main.py

--- myapp/main.py
+++ myapp/main.py
@@ -6,4 +6,5 @@ y=[3,4,5,6,7]
 
 fig=figure(plot_height=500,plot_width=1000)
 fig.line(x=x,y=y)
+fig.image_url(url=["myapp/static/images/base.png"],x=0,y=0,w=100,h=100,anchor="bottom_left")
 show(fig)
